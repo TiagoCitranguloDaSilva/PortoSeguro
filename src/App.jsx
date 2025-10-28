8eaf5171b77aeac2ba1ba2234b2598bc3b8edc86
Tiago Citrangulo da Silva <tiagocitrangulo256@gmail.com>
2025-10-28 15:45:51 -0300

    Começando o projeto e fazendo o CRUD de etiquetas

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,94 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css"
+import React, { useState, useEffect } from 'react'
+
+import EtiquetaForm from "./EtiquetaForm/EtiquetaForm"
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [etiquetas, setEtiquetas] = useState([])
+  const [idEtiqueta, setIdEtiqueta] = useState(-1)
+
+  const handleShowEtiquetaForm = (id = -1) => {
+    setIdEtiqueta(-1)
+    if(id != -1){
+      setIdEtiqueta(id)
+    }
+    document.querySelector("#formEtiqueta").style.display = "flex"
+  }
+
+  const handleEtiqueta = (valor) => {
+    if(idEtiqueta != -1){
+      etiquetas[idEtiqueta] = valor
+      setEtiquetas([...etiquetas])
+    }else{
+      setEtiquetas([...etiquetas, valor])
+    }
+  
+    setIdEtiqueta(-1)
+
+  }
+
+  
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h1>Porto Seguro</h1>
+      <div id="etiquetas">
+        <div className="headerContainer">
+          <h2>Etiquetas</h2>
+          <button onClick={() => handleShowEtiquetaForm()}>Nova etiqueta</button>
+        </div>
+        <div id="etiquetasHeader">
+          <h2>Etiquetas</h2>
+        </div>
+        <div id="etiquetasContainer">
+
+          {etiquetas.map((etiqueta, key) => {
+            return(
+            <div className="etiqueta" key={key} onClick={() => handleShowEtiquetaForm(key)}>
+              <p>{etiqueta}</p>
+            </div>)
+          })}
+
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div id="historias">
+        <div className="headerContainer">
+          <h2>Histórias</h2>
+          <button>Nova história</button>
+        </div>
+        <div className="historias">
+          <div className="historiaHeader">
+            <h2>Etiqueta</h2>
+          </div>
+          <div className="historiasContainer">
+
+            <div className="historia">
+              <p>Titulo</p>
+            </div>
+            <div className="historia">
+              <p>Titulo</p>
+            </div>
+            <div className="historia">
+              <p>Titulo</p>
+            </div>
+            <div className="historia">
+              <p>Titulo</p>
+            </div>
+            <div className="historia">
+              <p>Titulo</p>
+            </div>
+
+          </div>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    
+      <EtiquetaForm aoEnviar={handleEtiqueta} editarEtiqueta={etiquetas[idEtiqueta] ? etiquetas[idEtiqueta] : null} / >
+
     </>
   )
+
 }
 
 export default App
---
+++ src/EtiquetaForm/EtiquetaForm.jsx
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from 'react'
+import "./EtiquetaForm.css"
+
+function NovaEtiquetaForm({aoEnviar, editarEtiqueta}){
+
+    const [nomeEtiqueta, setNomeEtiqueta] = useState("")
+    const [editBool, setEditBool] = useState(false)
+
+    useEffect(() => {
+        if(editarEtiqueta != null){
+            setEditBool(true)
+            document.querySelector(".buttonApagar").style.display = 'block' 
+            setNomeEtiqueta(editarEtiqueta)
+        }else{
+            setEditBool(false)
+            document.querySelector(".buttonApagar").style.display = 'none' 
+        }
+    }, [editarEtiqueta])
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        aoEnviar(nomeEtiqueta)
+        fecharFormulario()
+    }
+
+    const fecharFormulario = () => {
+        document.querySelector("#formEtiqueta").style.display = "none"
+        setNomeEtiqueta("")
+    }
+
+    return(
+        <div id="formEtiqueta">
+            <form onSubmit={handleSubmit} autoComplete='off'>
+                <button onClick={fecharFormulario} id='fecharFormulario'>Fechar</button>
+                <h2>Nova etiqueta</h2>
+                <p><label htmlFor="nomeEtiqueta">Nome: </label><input type="text" id="nomeEtiqueta" required placeholder="Nome da etiqueta" value={nomeEtiqueta} maxLength={50} onChange={(e) => {setNomeEtiqueta(e.target.value)}} /></p>
+
+                <div className='botoes'>
+                    <input type="submit" value="Salvar" />
+                    <button className='buttonApagar'>Apagar</button>
+
+                </div>
+            </form>
+        </div>
+    )
+
+}
+
+export default NovaEtiquetaForm
