f98d8def98e2842319c4689f52be9390c1620d23
Tiago Citrangulo da Silva <tiagocitrangulo256@gmail.com>
2025-11-03 21:45:53 -0300

    Finalizando a versão beta

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,30 @@ import "./App.css"
 import React, { useState, useEffect } from 'react'
 
 import EtiquetaForm from "./EtiquetaForm/EtiquetaForm"
+import HistoriaForm from "./HistoriaForm/HistoriaForm"
 
 function App() {
 
   const [etiquetas, setEtiquetas] = useState([])
   const [idEtiqueta, setIdEtiqueta] = useState(-1)
+
   const [carregou, setCarregou] = useState(false)
+
+  const [historias, setHistorias] = useState([])
+  const [historiaSelecionada, setHistoriaSelecionada] = useState(null);
+  const [idHistoria, setIdHistoria] = useState(-1)
+  const [idEscolhaOriginal, setIdEscolhaOriginal] = useState(-1)
+
 
   useEffect(() => {
     if (!carregou) {
-      const armazenadas = JSON.parse(localStorage.getItem("etiquetas"));
-      if (armazenadas) {
-        setEtiquetas(armazenadas);
+      const etiquetasArmazenadas = JSON.parse(localStorage.getItem("etiquetas"));
+      const historiasArmazenadas = JSON.parse(localStorage.getItem("historias"));
+      if (etiquetasArmazenadas) {
+        setEtiquetas(etiquetasArmazenadas);
+        if(historiasArmazenadas){
+          setHistorias(historiasArmazenadas);
+        }
       }
       setCarregou(true);
     }
@@ -26,12 +38,30 @@ function App() {
     }
   }, [etiquetas, carregou])
 
+  useEffect(() => {
+    if (carregou) {
+      localStorage.setItem("historias", JSON.stringify(historias))
+    }
+  }, [historias, carregou])
+
   const handleShowEtiquetaForm = (id = -1) => {
     setIdEtiqueta(-1)
     if (id != -1) {
       setIdEtiqueta(id)
     }
     document.querySelector("#formEtiqueta").style.display = "flex"
+  }
+
+  const handleShowHistoriaForm = (id = -1, idEtiqueta = -1) => {
+    setHistoriaSelecionada(null)
+    setIdHistoria(-1)
+    if (id != -1) {
+      console.log("AAAAAAAAAAAAAAAAAAA")
+      setHistoriaSelecionada([historias[idEtiqueta][id], idEtiqueta])
+      setIdHistoria(id)
+      setIdEscolhaOriginal(idEtiqueta)
+    }
+    document.querySelector("#formHistoria").style.display = "flex"
   }
 
   const handleEtiqueta = (valor) => {
@@ -41,16 +71,49 @@ function App() {
       setEtiquetas([...temp])
     } else {
       setEtiquetas([...etiquetas, valor])
+      let tempHistorias = [...historias]
+      tempHistorias.push([])
+      setHistorias(tempHistorias)
     }
 
     setIdEtiqueta(-1)
 
   }
 
+  const handleHistoria = (titulo, historia, escolha) => {
+    let tempArrayHistoria = null
+    if(historiaSelecionada != null){
+      tempArrayHistoria = [...historias]
+      if(escolha == idEscolhaOriginal && escolha != -1){
+        tempArrayHistoria[escolha][idHistoria][0] = titulo
+        tempArrayHistoria[escolha][idHistoria][1] = historia
+      }else if(escolha != -1 && escolha != idEscolhaOriginal){
+        tempArrayHistoria[escolha].push([titulo, historia])
+        tempArrayHistoria[idEscolhaOriginal].splice(idHistoria, 1)
+      }
+    }else{
+      tempArrayHistoria = [...historias]
+      tempArrayHistoria[escolha].push([titulo, historia])
+    }
+    setHistorias(tempArrayHistoria)
+    setIdHistoria(-1)
+    setIdEscolhaOriginal(-1)
+  }
+
   const handleApagarEtiqueta = () => {
     let etiquetasAtualizadas = etiquetas.filter((_, index) => index !== idEtiqueta);
+    let historiasAtualizadas = historias.filter((_, index) => index != idEtiqueta)
     setEtiquetas(etiquetasAtualizadas)
+    setHistorias(historiasAtualizadas)
     setIdEtiqueta(-1)
+
+  }
+
+  const handleApagarHistoria = () => {
+    let historiaAtualizada = historias[idEscolhaOriginal].filter((_, index) => index !== idHistoria)
+    setHistorias(historiaAtualizada)
+    setIdHistoria(-1)
+    setIdEscolhaOriginal(-1)
 
   }
 
@@ -69,9 +132,9 @@ function App() {
         </div>
         <div id="etiquetasContainer">
 
-          {etiquetas.map((etiqueta, key) => {
+          {etiquetas.map((etiqueta, keyEtiqueta) => {
             return (
-              <div className="etiqueta" key={key} onClick={() => handleShowEtiquetaForm(key)}>
+              <div className="etiqueta" key={keyEtiqueta} onClick={() => handleShowEtiquetaForm(keyEtiqueta)}>
                 <p>{etiqueta}</p>
               </div>)
           })}
@@ -81,35 +144,35 @@ function App() {
       <div id="historias">
         <div className="headerContainer">
           <h2>Histórias</h2>
-          <button>Nova história</button>
+          <button onClick={() => handleShowHistoriaForm(-1)}>Nova história</button>
         </div>
         <div className="historias">
-          <div className="historiaHeader">
-            <h2>Etiqueta</h2>
-          </div>
-          <div className="historiasContainer">
-
-            <div className="historia">
-              <p>Titulo</p>
-            </div>
-            <div className="historia">
-              <p>Titulo</p>
-            </div>
-            <div className="historia">
-              <p>Titulo</p>
-            </div>
-            <div className="historia">
-              <p>Titulo</p>
-            </div>
-            <div className="historia">
-              <p>Titulo</p>
-            </div>
-
-          </div>
+          {etiquetas.map((etiqueta, key) => {
+            return(
+              
+                <div className="historiaContainer" key={key}>
+                  <div className="historiaHeader">
+                    <h2>{etiqueta}</h2>
+                  </div>
+                  <div className="historiasList">
+                    {historias[key]?.map((historia, keyHistoria) => {
+                      return(
+                        <div className="historia" onClick={() => handleShowHistoriaForm(keyHistoria, key)}>
+                          <p key={keyHistoria}>{historia[0]}</p>
+                        </div>
+                      )
+                    })}
+                  </div>
+                </div>
+            )
+          })}
+          
+          
         </div>
       </div>
 
       <EtiquetaForm aoEnviar={handleEtiqueta} editarEtiqueta={etiquetas[idEtiqueta] ? etiquetas[idEtiqueta] : null} aoClicar={handleApagarEtiqueta} />
+      <HistoriaForm aoEnviar={handleHistoria} editarHistoria={historiaSelecionada} aoClicar={handleApagarHistoria} etiquetas={etiquetas} />
 
     </>
   )
---
+++ src/HistoriaForm/HistoriaForm.jsx
@@ -0,0 +1,72 @@
+import React, { useState, useEffect } from 'react'
+import "./HistoriaForm.css"
+
+
+function HistoriaForm({aoEnviar, editarHistoria, aoClicar, etiquetas}){
+
+    const [nomeHistoria, setNomeHistoria] = useState("")
+    const [historia, setHistoria] = useState("")
+    const [escolha, setEscolha] = useState(0)
+
+
+    useEffect(() => {
+            if(editarHistoria != null){
+                document.querySelector("#formHistoria .buttonApagar").style.display = 'block' 
+                setNomeHistoria(editarHistoria[0][0])
+                setHistoria(editarHistoria[0][1])
+                setEscolha(editarHistoria[1])
+            }else{
+                document.querySelector("#formHistoria .buttonApagar").style.display = 'none' 
+            }
+        }, [editarHistoria])
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        aoEnviar(nomeHistoria, historia, escolha, )
+        fecharFormulario()
+    }
+
+    const fecharFormulario = () => {
+        document.querySelector("#formHistoria").style.display = "none"
+        setNomeHistoria("")
+        setHistoria("")
+        setEscolha(0)
+    }
+
+    return(
+        <div id="formHistoria">
+            <form onSubmit={handleSubmit} autoComplete='off'>
+                <button onClick={fecharFormulario} id='fecharFormulario'>Voltar</button>
+                <h2>Nova história</h2>
+                <div>
+                    <p><label htmlFor="tituloHistoria">Titulo: </label><input type="text" id="tituloHistoria" required placeholder="Nome da história" value={nomeHistoria} onChange={(e) => {setNomeHistoria(e.target.value)}} /></p>
+                    <p>
+                        <label htmlFor="escolhaEtiqueta">Etiqueta: </label>
+                        <select id="escolhaEtiqueta" onChange={(e) => setEscolha(document.querySelector("#escolhaEtiqueta").value)}>
+                            {etiquetas.map((etiqueta, index) => {
+                                return(
+                                    <option value={index} key={index} selected={index == escolha}>{etiqueta}</option>
+                                )
+                            })}
+                        </select>
+                    </p>
+                </div>
+                <p><label htmlFor="historiaTextArea">História: </label><textarea id="historiaTextArea" onChange={(e) => setHistoria(e.target.value)} value={historia} placeholder='Conte sua história'></textarea></p>
+                <div className='botoes'>
+                    <input type="submit" value="Salvar" />
+                    <button className='buttonApagar' onClick={
+                        (e) => {
+                            e.preventDefault()
+                            aoClicar()
+                            fecharFormulario()
+                        }
+                        }>Apagar</button>
+
+                </div>
+            </form>
+        </div>
+    )
+
+}
+
+export default HistoriaForm
